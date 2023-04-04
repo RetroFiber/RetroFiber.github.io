002b000c00f11e9f0b6806a93bb8a06dcf8882e1
Jonathan(RetroWolf)Howie <58430223+RetroWolf@users.noreply.github.com>
2023-04-04 14:59:17 -0400

    Update clockwork.js

--- clockwork-0.16.3/clockwork.js
+++ clockwork-0.16.3/clockwork.js
@@ -1,4 +1,4 @@
-alert(localStorage.getItem("theme"));
+localStorage.getItem("theme");
 if (localStorage.getItem("theme") == null) {
   localStorage.setItem("theme", "https://redstone-nw.netlify.app/clockwork-app/clockstyle.css")
   localStorage.setItem("facReset", "false");
