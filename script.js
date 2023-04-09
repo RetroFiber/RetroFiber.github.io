e3c3a7271a2d1b3afd13195edaa7455e2727a7e6
Jonathan(RetroWolf)Howie <58430223+RetroWolf@users.noreply.github.com>
2023-04-08 23:45:22 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/retrofiber.github.io/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+function selectbgColor() {
+         let bgcolor = document.getElementById('bgcolorInput');
+         // changing the background color
+         localStorage.setItem("bgc", bgcolor.value);
+         document.querySelector(':root').style.setProperty("--backgroundcolor", bgcolor.value);
+        }
+if (!(localStorage.getItem("bgc"))) { 
+  localStorage.setItem("bgc", "black"); 
+  document.querySelector(':root').style.setProperty("--backgroundcolor", "black");
+}
+document.querySelector(':root').style.setProperty("--backgroundcolor", localStorage.getItem('bgc'));
+
+function selectColor() {
+         let color = document.getElementById('colorInput');
+         // changing the background color
+         localStorage.setItem("color", color.value);
+         document.querySelector(':root').style.setProperty("--color", color.value);
+        }
+if (!(localStorage.getItem("color"))) { 
+  localStorage.setItem("color", "black"); 
+  document.querySelector(':root').style.setProperty("--color", "black");
+}
+document.querySelector(':root').style.setProperty("--color", localStorage.getItem('color'));
