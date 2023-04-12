32939a8c787f9e2c83947e9ca14165f6a85bf28d
Jonathan(RetroWolf)Howie <58430223+RetroWolf@users.noreply.github.com>
2023-04-12 10:10:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ if (!(localStorage.getItem("color"))) {
   document.querySelector(':root').style.setProperty("--color", "#63de00");
 }
 document.querySelector(':root').style.setProperty("--color", localStorage.getItem('color'));
+
+function black_white() {
+         localStorage.setItem("color", "black");
+         document.querySelector(':root').style.setProperty("--color", "black");
+         localStorage.setItem("bgc", "white");
+         document.querySelector(':root').style.setProperty("--backgroundcolor", "white");
+        }
+function white_garnet() {
+         localStorage.setItem("color", "white");
+         document.querySelector(':root').style.setProperty("--color", "white");
+         localStorage.setItem("bgc", "#2C0A21");
+         document.querySelector(':root').style.setProperty("--backgroundcolor", "#2C0A21");
+        }
+function white_black() {
+         localStorage.setItem("color", "white");
+         document.querySelector(':root').style.setProperty("--color", "white");
+         localStorage.setItem("bgc", "black");
+         document.querySelector(':root').style.setProperty("--backgroundcolor", "black");
+        }
+function green_black() {
+         localStorage.setItem("color", "#63de00");
+         document.querySelector(':root').style.setProperty("--color", "#63de00");
+         localStorage.setItem("bgc", "black");
+         document.querySelector(':root').style.setProperty("--backgroundcolor", "black");
+        }
