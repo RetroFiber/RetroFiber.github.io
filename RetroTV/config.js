79756c05b36f26df35da52271db2d63e2392075c
Jonathan(RetroWolf)Howie <58430223+RetroWolf@users.noreply.github.com>
2023-10-09 19:07:18 -0400

    Update config.js

--- RetroTV/config.js
+++ RetroTV/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://retromovie.mr-jmhowie.workers.dev/",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWU1MzgyN2Y3NDIwZTBiMzE4YzgxZDdkNTdhYTM3MCIsInN1YiI6IjY1MjQ4NjVmNzQ1MDdkMDBhYzRiNDI0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fk8a13lT6YxEXpwmuqu9_9wvrENpDCmCr45zxrUPics"
 };
